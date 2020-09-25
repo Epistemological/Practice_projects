2d993fcf48bc9526ea99cdd65893855e5add7ef9
Dennis Besseling <dennis.besseling@bisnode.com>
2020-09-25 15:14:23 +0200

    added examples of how to work with classes

--- training/classes/class_training.py
+++ training/classes/class_training.py
@@ -73,6 +73,7 @@ print(emp_1.pay)
 #3
 # class methods and static methods
 
+# CLASS METHODS
 class Employee:
 
     num_of_emps = 0
@@ -116,3 +117,93 @@ first, last, pay = emp_str_1.split('-')
 new_emp_1 = Employee(first, last, pay)
 new_emp_1.pay
 
+# STATIC METHODS
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = int(pay)
+        self.email = first + '.' + last + '@gmail.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+emp_1 = Employee('Dennis', 'Besseling', '50000')
+emp_2 = Employee('Mayra', 'Eriksson', '50000')
+
+import datetime
+my_date = datetime.date(2020, 9, 26)
+
+print(Employee.is_workday(my_date))
+
+
+# 4 Inheritance - Creating Subclasses
+
+class developer(Employee):
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+dev_1 = developer('Dennis', 'Besseling', '50000', 'python')
+dev_2 = developer('Mayra', 'Eriksson', '50000', 'java')
+
+print(help(developer))
+
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
+
+print(dev_1.email)
+print(dev_1.prog_lang)
+
+class manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+
+mgr_1 = manager('Sue', 'Smith', 90000, [dev_1])
